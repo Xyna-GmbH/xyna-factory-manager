44f614cef32e108dda8f935065ecfd693812a60a
Johannes Heucher <johannes.heucher@gip.com>
2023-08-07 14:30:01 +0200

    setting new patch-version before release

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const FactoryManagerName = 'Factory Manager';
-export const FactoryManagerVersion = '1.0.1';
+export const FactoryManagerVersion = '1.0.2';
