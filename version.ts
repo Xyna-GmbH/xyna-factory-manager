ec422d3d82214eb1862188a996dbe2a127d2f400
Johannes Heucher <johannes.heucher@gip.com>
2023-08-07 16:26:41 +0200

    preparing release v1.0.3

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const FactoryManagerName = 'Factory Manager';
-export const FactoryManagerVersion = '1.0.2';
+export const FactoryManagerVersion = '1.0.3';
